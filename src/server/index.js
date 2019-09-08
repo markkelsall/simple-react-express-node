77cbb99e6d36906d41a1a4a7b378a67cdfd6b123
Mark Kelsall <mark.kelsall@gmail.com>
2019-09-08 22:15:31 +0100

    remove unused endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -3,5 +3,4 @@ import express from 'express';
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getGists', (req, res) => res.send({ gists: [] }));
 app.listen(8080, () => console.log('Listening on port 8080!'));
