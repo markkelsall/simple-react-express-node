7e727c70819d0b8f4c67e999a7629863f9beed24
Mark Kelsall <mark.kelsall@gmail.com>
2019-09-09 21:58:48 +0100

    remove more old code for redux

--- src/client/actions/constants.js
+++
@@ -1,2 +0,0 @@
-export const GIT_GET_GISTS = 'GIT_GET_GISTS';
-export const GIT_GET_GISTS_RESPONSE = 'GIT_GET_GISTS_RESPONSE';
--- src/client/api/githubGistsService.js
+++
@@ -1,5 +0,0 @@
-const ENDPOINT = 'https://api.github.com/gists';
-
-export const fetchGists = () => {
-    return fetch(`${ENDPOINT}`).then(response => response.json());
-}
