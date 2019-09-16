9f0adf7e5ef9eb63cd2ac0b2fd763c90f947c0f3
Mark Kelsall <mark.kelsall@gmail.com>
2019-09-16 20:44:06 +0100

    add a change so we can tell it's changing

--- src/client/App.js
+++ src/client/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-          <h1 className="hello">Hello!</h1>
+          <h1 className="hello">Hello! 1</h1>
       </div>
     );
   }
